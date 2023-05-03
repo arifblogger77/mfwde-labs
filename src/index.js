59e8b5c1ef9ba5ab06d5496f38b5c0a27aae8d45
Arifblogger77 <arifblogger77@gmail.com>
2023-05-03 14:39:04 +0700

    Latihan: menerapkan teks optimisasi dan off canvas menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './styles/main.css';
+
+const hamburgerButtonElement = document.querySelector("#hamburger")
+const drawerElement = document.querySelector("#drawer")
+const heroElement = document.querySelector(".hero")
+const mainElement = document.querySelector("main")
+
+hamburgerButtonElement.addEventListener('click', event => {
+    drawerElement.classList.toggle('open');
+    event.stopPropagation();
+})
+
+mainElement.addEventListener('click', event => {
+    drawerElement.classList.remove('open');
+    event.stopPropagation();
+})
+
+heroElement.addEventListener('click', event => {
+    drawerElement.classList.remove('open');
+    event.stopPropagation();
+})
